874605cceac4d798bbf3a80ac32117d28e0a93a6
MatthewIczkowski <iczkom13@gmail.com>
2024-01-24 08:53:00 -0700

    pushed elements to bottom of demo page, updated buttons, added card images

--- pages/Demo.js
+++ pages/Demo.js
@@ -1,30 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView, StyleSheet, Text, View, Image, Pressable, Button } from 'react-native';
 
+const CardImage = require('../assets/face-down-card.png');
+
 export default function Demo() {
   return (
-    <SafeAreaView style={styles.container}>
+    <SafeAreaView style={{flex: 1, backgroundColor: '#016040', alignItems: 'center'}}>
       <Text style={{fontSize: 30, fontWeight: 'bold', color: '#fff'}}>3 Putt Poker</Text>
-      <Text style={{fontSize: 24, color: "#ffd33d"}}>Select Player</Text>
-      <View style={{flexDirection: 'row', justifyContent: 'space-around'}}>
-        <Button
-          title="Player 1"
-          onPress={() => alert('You pressed a button.')}
-        />
-        <Button
-          title="Player 2"
-          onPress={() => alert('You pressed a button.')}
-        />
-      </View>
-      <View style={{flexDirection: 'row'}}>
-        <Button
-          title="Player 3"
-          onPress={() => alert('You pressed a button.')}
-        />
-        <Button
-          title="Player 4"
-          onPress={() => alert('You pressed a button.')}
-        />
+      <View style={styles.container}>
+        <View style={styles.imageContainer}>
+          <Image source={CardImage} style={styles.image} resizeMode='contain' />
+          <Image source={CardImage} style={styles.image} resizeMode='contain' />
+        </View>
+        <Text style={{fontSize: 24, color: "#ffd33d"}}>Select Player</Text>
+        <View style={{flexDirection: 'row', margin:'20px', gap: '20px'}}>
+          <View  style={[styles.buttonContainer, { borderWidth: 3, borderColor: "#ffd33d", borderRadius: 18 }]}>
+            <Pressable style={styles.button} onPress={() => alert('You pressed a button.')}>
+            <Text style={styles.buttonLabel}>Player 1</Text>
+            </Pressable>
+          </View>
+          <View  style={[styles.buttonContainer, { borderWidth: 3, borderColor: "#ffd33d", borderRadius: 18 }]}>
+            <Pressable style={styles.button} onPress={() => alert('You pressed a button.')}>
+            <Text style={styles.buttonLabel}>Player 2</Text>
+            </Pressable>
+          </View>
+        </View>
+        <View style={{flexDirection: 'row', gap: '20px', marginBottom: '20px'}}>
+          <View  style={[styles.buttonContainer, { borderWidth: 3, borderColor: "#ffd33d", borderRadius: 18 }]}>
+            <Pressable style={styles.button} onPress={() => alert('You pressed a button.')}>
+            <Text style={styles.buttonLabel}>Player 3</Text>
+            </Pressable>
+          </View>
+          <View  style={[styles.buttonContainer, { borderWidth: 3, borderColor: "#ffd33d", borderRadius: 18 }]}>
+            <Pressable style={styles.button} onPress={() => alert('You pressed a button.')}>
+            <Text style={styles.buttonLabel}>Player 4</Text>
+            </Pressable>
+          </View>
+        </View>
       </View>
       <StatusBar style="auto" />
     </SafeAreaView>
@@ -36,21 +48,24 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#016040',
     alignItems: 'center',
+    justifyContent: 'flex-end',
   },
   imageContainer:{
     flex: 1,
-    paddingTop: 58,
+    flexDirection: 'row', 
+    margin:'10px', 
+    gap: '5px',
+    paddingTop: '40px',
   },
   image: {
-    width: 320,
-    height: 440,
-    borderRadius: 18,
+    width: 200,
+    height: 240,
   },
   buttonContainer: {
-    width: 200,
-    height: 68,
-    marginHorizontal: 20,
-    marginBottom: 20,
+    width: 100,
+    height: 50,
+    marginHorizontal: 10,
+    marginBottom: 10,
     alignItems: 'center',
     justifyContent: 'center',
     padding: 3,
